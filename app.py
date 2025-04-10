c5616cb8c9bcdb431dd56c5e2ef12e3253799c2c
Khushi-tw <khushit355@gmail.com>
2025-04-10 12:48:38 +0530

    Upadated app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 
 from flask import Flask, render_template,jsonify
+from database import load_jobs_from_db
+from sqlalchemy import text
 
 app = Flask(__name__, template_folder='templates')
 
-JOBS = [
-    {'id': 1, 'title': 'Data Analyst', 'location': 'Bengaluru, India', 'salary': 'Rs 10,00,000'},
-    {'id': 2, 'title': 'Data Scientist', 'location': 'Delhi, India', 'salary': 'Rs 15,00,000'},
-    {'id': 3, 'title': 'Data Engineer', 'location': 'Bengaluru, India', 'salary': 'Rs 12,00,000'},
-    {'id': 4, 'title': 'Frontend Developer', 'location': 'Bengaluru, India', 'salary': 'Rs 12,00,000'},
 
-]
+
 
 @app.route("/")
-def hello_world():
-    return render_template("home.html", jobs=JOBS,company_name = 'NextGen')
+def hello_World():
+    jobs_list = load_jobs_from_db()
+    return render_template("home.html", Jobs=jobs_list,company_name = 'NextGen')
 
-@app.route("/api/jobs")
+@app.route("/api/jobs_list")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs_list = load_jobs_from_db()
+    return jsonify(jobs_list)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
